e91f435426f25dc5eac28c58ca9f24b29f4074c5
lauu-petite <olmedoriveralaura@gmail.com>
2026-05-29 13:28:51 +0200

    añadir siglo a quizzes respuesta

--- AtlasAPI/Models/Partida.cs
+++ AtlasAPI/Models/Partida.cs
@@ -8,6 +8,7 @@ namespace AtlasAPI.Models
         public int UsuarioId { get; set; }
         public int Aciertos { get; set; }
         public int TotalPreguntas { get; set; }
+        public int Siglo { get; set; }
         public DateTime Fecha { get; set; } = DateTime.Now;
 
         // Relación detallada de respuestas
